b6edd4ab602e6a09944ab6e346a9b853c05cf7b4
Mark Santcroos <mas781@scarletmail.rutgers.edu>
2015-01-02 12:18:26 +0100

    Use the configdir directive.

--- init.lua
+++ init.lua
@@ -191,6 +191,6 @@ end
 function reload_config(files)
     hs.reload()
 end
-hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reload_config):start()
+hs.pathwatcher.new(hs.configdir, reload_config):start()
 hs.alert.show("Config Re-loaded")
 --
